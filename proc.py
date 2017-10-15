8e0abd1f50c03b9c58748d93269bc1c35c59fbb2
Issa Rice <riceissa@gmail.com>
2017-10-15 14:54:35 -0700

    progress of finding the right table

--- proc.py
+++ proc.py
@@ -8,7 +8,7 @@ url = "https://timelines.issarice.com/api.php?action=parse&page=Timeline_of_The_
 
 payload = {
     "action": "parse",
-    "page": "Timeline of Airbnb",
+    "page": "Timeline of brain cancer",
     "format": "json",
 }
 
@@ -20,3 +20,33 @@ tables = soup.find_all("table")
 
 # subtract one from number of rows to compensate for header row
 print(list(map(lambda t: len(t.find_all("tr")) - 1, tables)))
+
+# soup.find_all("h2")[1].find("span", {"class": "mw-headline"}).text
+# soup.find_all("h2")[1].next_sibling.next_sibling.next_sibling.next_sibling
+
+def full_timeline_heading(soup):
+    """Find and return the "Full timeline" heading."""
+    for h2 in soup.find_all("h2"):
+        span = h2.find("span", {"class": "mw-headline"})
+        if span and span.text == "Full timeline":
+            return h2
+
+def full_timeline_table(soup, h2):
+    """
+    Find and return the "Full timeline" table tag given the tag soup and the h2
+    "Full timeline" heading tag.
+    """
+    tag = h2
+    while tag and tag.name != "table":
+        tag = tag.next_sibling
+    if tag:
+        return tag
+
+    # Otherwise, we stepped right through the page without finding the table
+
+h2 = full_timeline_heading(soup)
+full_timeline = full_timeline_table(soup, h2)
+if full_timeline:
+    print("Number of rows", len(full_timeline.find_all("tr")) - 1)
+else:
+    print("Could not find full timeline.")
